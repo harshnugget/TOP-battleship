f27d7671a7ed0f175ecbe2f7d2ee0330ffcb5a6d
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-10-17 14:42:13 +0100

    Initial commit

---
+++ webpack.common.js
@@ -0,0 +1,35 @@
+import { resolve } from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const config = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    clean: true,
+  },
+};
+
+export default config;
