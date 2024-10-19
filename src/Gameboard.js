5f0ea3cefd45c1e36619786385e00066df5c69ce
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-10-19 04:47:08 +0100

    feat(Gameboard.js): create Gameboard class to manage gameboard logic
    - Implemented the Gameboard class with methods for creating a board, placing ships, and receiving attacks.
    - validateCoordinates() method to ensure proper ship placement and attack handling.
    - allShipsSunk() method to check if all ships are sunk.
    - removeShip() method for removing a ship from the board
    - Included unit tests

---
+++ src/Gameboard.js
@@ -0,0 +1,158 @@
+class Gameboard {
+  #coordinates;
+  #missedShots;
+  #shipPos;
+
+  constructor(size) {
+    this.#coordinates = [];
+    this.#missedShots = [];
+    this.#shipPos = new Map(); // Keeps track of ship coordinates
+
+    this.createBoard(size); // Initialize a size*size board
+  }
+
+  get coordinates() {
+    return [...this.#coordinates];
+  }
+
+  get missedShots() {
+    return [...this.#missedShots];
+  }
+
+  get shipPos() {
+    return new Map(this.#shipPos);
+  }
+
+  createBoard(size) {
+    // Validate size (must be a positive integer)
+    if (!Number.isInteger(size) || size <= 0) {
+      throw new Error('Invalid board size! Must be a positive integer.');
+    }
+
+    // Clear current board data
+    this.#coordinates = [];
+    this.#missedShots = [];
+    this.#shipPos.clear();
+
+    // Initialize each coordinate as an object that contains values for a ship and a hit
+    for (let row = 0; row < size; row++) {
+      this.#coordinates.push([]);
+      for (let column = 0; column < size; column++) {
+        this.#coordinates[row].push({ ship: null, hit: false });
+      }
+    }
+  }
+
+  validateCoordinates(coordinates) {
+    const row = coordinates[0];
+    const col = coordinates[1];
+
+    if (
+      row < 0 ||
+      row > this.#coordinates.length - 1 ||
+      col < 0 ||
+      col > this.#coordinates[0].length - 1
+    ) {
+      throw Error('Coordinates out of bounds!');
+    }
+
+    if (this.#coordinates[row][col].hit === true) {
+      throw Error('These coordinates have already been attacked!');
+    }
+
+    if (this.#coordinates[row][col].ship !== null) {
+      throw Error('These coordinates are already occupied!');
+    }
+  }
+
+  placeShip(ship, coordinates, orientation) {
+    // Check that the ship isn't already on the board
+    if (this.#shipPos.get(ship)) {
+      throw Error(
+        `This ship already exists on the board! Remove the ship before placing it again.`
+      );
+    }
+
+    const startRow = coordinates[0];
+    const startCol = coordinates[1];
+    const validCoordinates = []; // Coordinates the ship will occupy
+
+    const storeValidCoordinates = (row, col) => {
+      try {
+        this.validateCoordinates([row, col]);
+        validCoordinates.push([row, col]);
+      } catch (error) {
+        throw Error(`Could not place the ship!\n${error.message}`);
+      }
+    };
+
+    if (orientation === 'horizontal') {
+      const endCol = startCol + ship.length;
+
+      // Increment columns until greater than endCol
+      for (let currentCol = startCol; currentCol < endCol; currentCol++) {
+        storeValidCoordinates(startRow, currentCol);
+      }
+    } else if (orientation === 'vertical') {
+      const endRow = startRow - ship.length;
+
+      // Decrement rows until less than endRow
+      for (let currentRow = startRow; currentRow > endRow; currentRow--) {
+        storeValidCoordinates(currentRow, startCol);
+      }
+    } else {
+      throw Error('Invalid orientation! Must be either vertical or horizontal');
+    }
+
+    // Assign the ship to each validated coordinate
+    validCoordinates.forEach(([row, col]) => {
+      this.#coordinates[row][col].ship = ship;
+    });
+
+    this.#shipPos.set(ship, validCoordinates);
+  }
+
+  removeShip(ship) {
+    const coordinatesToReset = this.#shipPos.get(ship);
+
+    if (!coordinatesToReset) {
+      throw Error('This ship does not exist on the board!');
+    }
+
+    coordinatesToReset.forEach(([row, col]) => (this.#coordinates[row][col].ship = null));
+    this.#shipPos.delete(ship);
+  }
+
+  receiveAttack(coordinates) {
+    const row = coordinates[0];
+    const col = coordinates[1];
+    const ship = this.#coordinates[row][col].ship;
+
+    if (this.#coordinates[row][col].hit === true) {
+      throw Error('These coordinates have already been hit!');
+    }
+
+    this.#coordinates[row][col].hit = true;
+
+    if (ship !== null) {
+      ship.hit();
+      return true;
+    }
+
+    this.#missedShots.push([row, col]);
+    return false;
+  }
+
+  allShipsSunk() {
+    const ships = Array.from(this.#shipPos.keys());
+
+    if (ships.length === 0) {
+      throw Error('Cannot check for sunk ships! There are no ships on the board.');
+    }
+
+    // Returns true if all ships are sunk
+    return ships.every((ship) => ship.isSunk);
+  }
+}
+
+export default Gameboard;
---
+++ src/Gameboard.test.js
@@ -0,0 +1,159 @@
+import Gameboard from './Gameboard';
+import { jest } from '@jest/globals';
+
+describe('Gameboard', () => {
+  let gameboard;
+
+  // Factory function for creating mock ship objects
+  const mockShip = (length) => {
+    return {
+      length,
+      hits: 0,
+      get isSunk() {
+        return this.hits >= this.length;
+      },
+      hit: jest.fn().mockImplementation(function () {
+        this.hits++;
+      }),
+    };
+  };
+
+  beforeEach(() => {
+    gameboard = new Gameboard(10); // Create a new gameboard of size 10
+  });
+
+  test('Create a gameboard with an invalid size', () => {
+    expect(() => gameboard.createBoard(-10)).toThrow();
+    expect(() => gameboard.createBoard('ten')).toThrow();
+  });
+
+  test('Expected gameboard size', () => {
+    const checkSize = (size) => {
+      gameboard.createBoard(size);
+      expect(gameboard.coordinates.length).toBe(size); // Number of rows
+      expect(gameboard.coordinates[0].length).toBe(size); // Number of columns
+    };
+
+    checkSize(6);
+    checkSize(10);
+  });
+
+  test('Invalid coordinate boundaries', () => {
+    // Expect out of bounds coordinates to throw an error
+    const outOfBounds = [
+      [-1, 0],
+      [0, -1],
+      [10, 0],
+      [0, 10],
+    ];
+
+    outOfBounds.forEach((coord) => {
+      expect(() => gameboard.validateCoordinates(coord)).toThrow();
+    });
+  });
+
+  test('Placing a ship horizontally', () => {
+    const ship = mockShip(2);
+
+    gameboard.placeShip(ship, [0, 1], 'horizontal');
+    expect(gameboard.coordinates[0][0].ship).not.toBe(ship);
+    expect(gameboard.coordinates[0][1].ship).toBe(ship);
+    expect(gameboard.coordinates[0][2].ship).toBe(ship);
+    expect(gameboard.coordinates[0][3].ship).not.toBe(ship);
+
+    expect(gameboard.shipPos.get(ship).length).toBe(2);
+
+    // Attempt to place a ship that overflows bounds
+    expect(() => gameboard.placeShip(mockShip(3), [1, 8], 'horizontal')).toThrow('out of bounds');
+  });
+
+  test('Placing a ship vertically', () => {
+    const ship = mockShip(2);
+
+    gameboard.placeShip(ship, [3, 0], 'vertical');
+    expect(gameboard.coordinates[4][0].ship).not.toBe(ship);
+    expect(gameboard.coordinates[3][0].ship).toBe(ship);
+    expect(gameboard.coordinates[2][0].ship).toBe(ship);
+    expect(gameboard.coordinates[1][0].ship).not.toBe(ship);
+
+    expect(gameboard.shipPos.get(ship).length).toBe(2);
+
+    // Attempt to place a ship that overflows bounds
+    expect(() => gameboard.placeShip(mockShip(5), [3, 1], 'vertical')).toThrow('out of bounds');
+  });
+
+  test('Invalid ship placement', () => {
+    const ship = mockShip(2);
+
+    expect(() => gameboard.placeShip(ship, [0, 9], 'horizontal')).toThrow();
+    expect(() => gameboard.placeShip(ship, [0, 0], 'vertical')).toThrow();
+  });
+
+  test('Receiving an attack at empty coordinates', () => {
+    expect(gameboard.coordinates[0][0].hit).toBe(false);
+    gameboard.receiveAttack([0, 0]);
+    expect(gameboard.coordinates[0][0].hit).toBe(true);
+
+    expect(gameboard.coordinates[0][1].hit).toBe(false);
+    gameboard.receiveAttack([0, 1]);
+    expect(gameboard.coordinates[0][1].hit).toBe(true);
+
+    // Ensure the correct amount of missed shots have been tracked
+    expect(gameboard.missedShots.length).toBe(2);
+
+    // Try to attack coordinates that have already been hit
+    expect(() => gameboard.receiveAttack([0, 0])).toThrow();
+  });
+
+  test('Receiving an attack at ship coordinates', () => {
+    const ship = mockShip(2);
+
+    // Try to place ship at coordinates that have already been hit
+    gameboard.receiveAttack([0, 0]);
+    expect(() => gameboard.placeShip(ship, [0, 0], 'horizontal')).toThrow();
+
+    // Place the ship at empty coordinates
+    gameboard.placeShip(ship, [1, 0], 'horizontal');
+
+    // Attack the coordinates occupied by the ship
+    gameboard.receiveAttack([1, 0]);
+    expect(gameboard.coordinates[1][0].hit).toBe(true);
+    expect(ship.hit).toHaveBeenCalledTimes(1);
+  });
+
+  test('Sinking all ships', () => {
+    const ship1 = mockShip(1);
+    const ship2 = mockShip(1);
+
+    gameboard.placeShip(ship1, [0, 0], 'horizontal');
+    gameboard.placeShip(ship2, [1, 0], 'horizontal');
+
+    expect(gameboard.allShipsSunk()).toBe(false);
+
+    gameboard.receiveAttack([0, 0]);
+
+    expect(gameboard.allShipsSunk()).toBe(false);
+
+    gameboard.receiveAttack([1, 0]);
+
+    expect(gameboard.allShipsSunk()).toBe(true);
+  });
+
+  test('Removing a ship from the board', () => {
+    const ship = mockShip(3);
+
+    gameboard.placeShip(ship, [0, 0], 'horizontal');
+
+    expect(gameboard.shipPos.has(ship)).toBe(true);
+
+    // Remove the ship
+    gameboard.removeShip(ship);
+    expect(gameboard.shipPos.has(ship)).toBe(false);
+    expect(gameboard.coordinates[0][0].ship).toBe(null);
+    expect(gameboard.coordinates[0][1].ship).toBe(null);
+    expect(gameboard.coordinates[0][2].ship).toBe(null);
+
+    // Remove a ship that doesn't exist on the board
+    expect(() => gameboard.removeShip({})).toThrow();
+  });
+});
