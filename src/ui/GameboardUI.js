1f93579ff858998e1fc2163f71fa6ab341d8c684
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-11-15 01:50:22 +0000

    refactor(GameboardUI.js): Simplified element creation and UI logic
    - `createGameboardElement` function now only requires a size argument, which will be obtained from the gameboard object
    - Replaced `cellSize`, `rows`, `columns` properties with a `gameboard` object passed to the constructor
    - Removed methods `traverseCells`, `placeShip` and `isValidPlacement` which are no longer needed
    - Added methods for retrieving all cells (`getAllCells``) and hit cells (`getHitCells`)
    - Simplified cell color management with `cellColours` for hit, miss, and ship states
    - Refactored rendering logic to iterate over the gameboard object's coordinates and update cell states directly

--- src/ui/GameboardUI.js
+++ src/ui/GameboardUI.js
@@ -1,17 +1,18 @@
 /* This function creates an element for displaying the gameboard grid
 - Each cell will represent a coordinate (e.g. [3, 4]) on the gameboard */
 
-const createGameboardElement = (cellSize, rows, columns) => {
+const createGameboardElement = (size) => {
+  const cellSize = 40;
   const element = document.createElement('div');
   element.classList.add('gameboard');
 
   element.style.display = 'grid';
-  element.style.gridTemplateColumns = `repeat(${columns}, ${cellSize}px)`;
-  element.style.gridTemplateRows = `repeat(${rows}, ${cellSize}px)`;
+  element.style.gridTemplateColumns = `repeat(${size}, ${cellSize}px)`;
+  element.style.gridTemplateRows = `repeat(${size}, ${cellSize}px)`;
   element.style.border = '1px solid black';
   element.style.width = 'max-content';
   element.style.height = 'max-content';
-  element.style.gap = '2px';
+  element.style.gap = '1px';
 
   const createCellContainer = () => {
     const element = document.createElement('div');
@@ -20,8 +21,8 @@ const createGameboardElement = (cellSize, rows, columns) => {
     return element;
   };
 
-  for (let row = rows - 1; row >= 0; row--) {
-    for (let col = 0; col < columns; col++) {
+  for (let row = size - 1; row >= 0; row--) {
+    for (let col = 0; col < size; col++) {
       const cellContainer = createCellContainer();
       cellContainer.dataset.row = row;
       cellContainer.dataset.column = col;
@@ -34,194 +35,81 @@ const createGameboardElement = (cellSize, rows, columns) => {
 
 class GameboardUI {
   #parentContainer;
-  #cellSize;
-  #rows;
-  #columns;
   #gameboardElement;
+  #gameboard;
 
-  static cellColours = {
-    placeholder: 'grey',
-    hit: 'red',
-    ship: 'blue',
-    hitShip: 'dark-blue',
-    empty: '',
-  };
-
-  constructor(parentContainer, cellSize = 40, rows = 10, columns = 10) {
+  constructor(gameboard, parentContainer) {
     this.#parentContainer = parentContainer;
-    this.#cellSize = cellSize;
-    this.#rows = rows;
-    this.#columns = columns;
-    this.#gameboardElement = createGameboardElement(cellSize, rows, columns);
+    this.#gameboardElement = createGameboardElement(gameboard.size);
     this.#parentContainer.append(this.#gameboardElement);
+    this.#gameboard = gameboard;
+    this.hidden = false;
+
+    this.cellColours = {
+      ship: 'blue',
+      hit: 'darkblue',
+      miss: 'red',
+      empty: '',
+    };
   }
 
   get parentContainer() {
     return this.#parentContainer;
   }
 
-  get cellSize() {
-    return this.#cellSize;
-  }
-
   get gameboardElement() {
     return this.#gameboardElement;
   }
 
+  get gameboard() {
+    return this.#gameboard;
+  }
+
   getCell(coordinates) {
-    // Return the cell element at specified coordinates
+    // Return the cell at specified coordinates
     return this.#gameboardElement.querySelector(
       `[data-row="${coordinates[0]}"][data-column="${coordinates[1]}"]`
     );
   }
 
+  getHitCells() {
+    return this.#gameboardElement.querySelectorAll(`:scope > .cell.hit`);
+  }
+
   getAllCells() {
-    return this.#gameboardElement.querySelectorAll('.cell');
+    return this.#gameboardElement.querySelectorAll(`:scope > .cell`);
   }
 
-  traverseCells(startCoordinates, orientation, callback, limit = this.#rows * this.#columns) {
-    if (limit < 1 || limit > this.#rows * this.#columns) {
-      return console.error(`Limit must be between 1 and ${this.#rows * this.#columns}.`);
-    }
+  render() {
+    this.#gameboard.coordinates.forEach((row, rowIndex) =>
+      row.forEach((col, colIndex) => {
+        const cell = this.getCell([rowIndex, colIndex]);
 
-    const [startRow, startCol] = startCoordinates;
-
-    const cells = [];
-    let i = 0;
-
-    if (orientation === 'vertical') {
-      // Traverses from top right to bottom left
-      for (let col = this.#columns - 1; col >= 0; col--) {
-        for (let row = this.#rows - 1; row >= 0; row--) {
-          if ((row > startRow && col === startCol) || col > startCol) {
-            continue;
-          }
-
-          if (i < limit) {
-            const cell = this.getCell([row, col]);
-            cells.push(cell);
-            i++;
-          }
+        // Add/remove classes to cells
+        if (col.hit) {
+          cell.classList.add('hit');
+        } else {
+          cell.classList.remove('hit');
         }
-      }
-    } else if (orientation === 'horizontal') {
-      // Traverses from bottom left to top right
-      for (let row = 0; row < this.#rows; row++) {
-        for (let col = 0; col < this.#columns; col++) {
-          if ((col < startCol && row === startRow) || row < startRow) {
-            continue;
-          }
 
-          if (i < limit) {
-            const cell = this.getCell([row, col]);
-            cells.push(cell);
-            i++;
-          }
+        if (col.ship) {
+          cell.classList.add('ship');
+        } else {
+          cell.classList.remove('ship');
         }
-      }
-    } else {
-      throw new Error(`Orientation must be "vertical" or "horizontal"`);
-    }
 
-    // Apply the callback to each cell
-    cells.forEach(callback);
-    return cells;
-  }
-
-  receiveAttack(coordinates) {
-    const cell = this.getCell(coordinates);
-
-    if (cell) {
-      cell.classList.add('hit-cell');
-    }
-
-    this.updateCells(GameboardUI.cellColours);
-    return cell;
-  }
-
-  placeShip(coordinates, length, orientation = 'horizontal', remove = false) {
-    let cells;
-
-    // Validate ship placement
-    if (this.isValidPlacement(coordinates, length, orientation)) {
-      cells = this.traverseCells(
-        coordinates,
-        orientation,
-        (cell) => {
-          if (remove) {
-            cell.classList.remove('ship-cell');
-          } else {
-            cell.classList.add('ship-cell');
-          }
-        },
-        length
-      );
-    } else {
-      throw new Error('Invalid ship placement');
-    }
-
-    this.updateCells(GameboardUI.cellColours);
-    return cells;
-  }
-
-  shipPlaceholder(coordinates, length, orientation = 'horizontal') {
-    let cells;
-
-    // Deselect current placeholders
-    const placeholderCells = this.#gameboardElement.querySelectorAll('.cell.placeholder');
-
-    placeholderCells.forEach((cell) => {
-      cell.classList.remove('placeholder');
-    });
-
-    cells = this.traverseCells(
-      coordinates,
-      orientation,
-      (cell) => {
-        cell.classList.add('placeholder');
-      },
-      length
+        // Add styles to cells
+        if (col.hit && col.ship) {
+          cell.style.backgroundColor = this.cellColours.hit;
+        } else if (col.ship && this.hidden === false) {
+          cell.style.backgroundColor = this.cellColours.ship;
+        } else if (col.hit) {
+          cell.style.backgroundColor = this.cellColours.miss;
+        } else {
+          cell.style.backgroundColor = this.cellColours.empty;
+        }
+      })
     );
-
-    this.updateCells(GameboardUI.cellColours);
-    return cells;
-  }
-
-  isValidPlacement(coordinates, length, orientation) {
-    const [row, col] = coordinates;
-
-    // Validate coordinates
-    if (row < 0 || row >= this.#rows || col < 0 || col >= this.#columns) {
-      return false;
-    }
-
-    // Check boundaries based on length and orientation
-    switch (orientation) {
-      case 'horizontal':
-        return col + length <= this.#columns;
-      case 'vertical':
-        return row - (length - 1) >= 0;
-      default:
-        return false; // Invalid orientation
-    }
-  }
-
-  updateCells(colours = { placeholder, hit, ship, hitShip, empty }) {
-    const allCells = this.#gameboardElement.querySelectorAll('.cell');
-
-    allCells.forEach((cell) => {
-      if (cell.classList.contains('placeholder')) {
-        cell.style.backgroundColor = colours.placeholder;
-      } else if (cell.classList.contains('ship-cell') && cell.classList.contains('hit-cell')) {
-        cell.style.backgroundColor = colours.hitShip;
-      } else if (cell.classList.contains('ship-cell')) {
-        cell.style.backgroundColor = colours.ship;
-      } else if (cell.classList.contains('hit-cell')) {
-        cell.style.backgroundColor = colours.hit;
-      } else {
-        cell.style.backgroundColor = colours.empty;
-      }
-    });
   }
 }
 
