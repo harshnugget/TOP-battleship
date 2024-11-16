106dea84b5556012cd34585bedcddf924dffde75
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-11-16 00:27:28 +0000

    feat(Battleship.js): Integrate game logic for gameboard, players, ships, and game controller
    - Implemented the Battleship class that integrates the game's core functionality.
    - Implemented a default ship configuation to be used in player creation.
    - Implemented a static method to create players and initialize their ships and gameboards.
    
    Parameters:
    - Player names to be used in player creation.
    - `logMessages` boolean for logging game interactions and errors to the console.
    
    - `controller` will handle game flow through an initialized `GameController`.
    - `logMessage` will display a message in the console if the `logMessages` boolean is set to true.
    - Implemented a `formatErrorMsg` method for formatting error messages to be displayed in the console.
    - Implemented a `prompt` method to display the next user action to be taken.
    - Implemented methods for starting and resetting the game.
    - Implemented `attack` method for attacking at specified coordinates.
    - Implemented `printBoard` method with optional hiding of ships to represent the current state of a players board.
    - Attacking will print a representation of each player's board to the console.
    - Provided functionality for placing and resetting individual and all ships for each player.

---
+++ src/Battleship.js
@@ -0,0 +1,182 @@
+import Ship from './logic/Ship.js';
+import Gameboard from './logic/Gameboard.js';
+import Player from './logic/Player.js';
+import GameController from './logic/GameController.js';
+
+class Battleship {
+  #players;
+  #controller;
+
+  static shipConfig = [
+    { type: 'destroyer', length: 2 },
+    { type: 'submarine', length: 3 },
+    { type: 'cruiser', length: 3 },
+    { type: 'battleship', length: 4 },
+    { type: 'carrier', length: 5 },
+  ];
+
+  static createPlayer = (name, id) => {
+    const gameboardShips = [];
+    const playerShips = {};
+
+    Battleship.shipConfig.forEach(({ type, length }) => {
+      const ship = new Ship(length);
+
+      gameboardShips.push(ship);
+      playerShips[type] = { ship };
+    });
+
+    const gameboard = new Gameboard(10, gameboardShips);
+    const player = new Player(name, gameboard);
+
+    return { id, ships: playerShips, gameboard, player };
+  };
+
+  constructor(player1Name, player2Name, logMessages = false) {
+    this.#players = [
+      Battleship.createPlayer(player1Name, 1),
+      Battleship.createPlayer(player2Name, 2),
+    ];
+
+    this.#controller = new GameController(this.#players[0].player, this.#players[1].player);
+    this.logMessages = Boolean(logMessages);
+  }
+
+  logMessage(message, level = 'info') {
+    if (this.logMessages && message) {
+      switch (level) {
+        case 'warn':
+          console.warn(message);
+          break;
+        case 'error':
+          console.error(message);
+          break;
+        default:
+          console.log(message);
+      }
+    }
+  }
+
+  formatErrorMsg(error) {
+    if (!error || !error.message) {
+      return 'An unknown error occurred.';
+    }
+
+    return `${error.message}${error.cause ? `\n${error.cause.message}` : ''}`;
+  }
+
+  prompt() {
+    if (this.#controller.gameInProgress === true) {
+      this.logMessage(
+        `Waiting for Player ${this.getPlayerId(this.#controller.activePlayer)} to attack: `
+      );
+    } else {
+      this.logMessage('Waiting for game to begin.');
+    }
+  }
+
+  startGame() {
+    try {
+      this.#controller.startGame();
+      this.logMessage('Game started.');
+      this.prompt();
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  resetGame() {
+    try {
+      this.#controller.resetGame();
+      this.resetAllShips(1);
+      this.resetAllShips(2);
+      this.logMessage('Game reset.');
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  attack(row, col) {
+    try {
+      const activePlayer = this.#controller.activePlayer;
+      const hit = this.#controller.attack([row, col]);
+      const message = `Player ${this.getPlayerId(activePlayer)} ${hit ? 'hit' : 'missed'} a target at: [${row}, ${col}]`;
+      this.printBoard(1, true);
+      this.printBoard(2, true);
+      this.logMessage(`${message}`);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+
+    if (this.#controller.gameHasWinner()) {
+      this.logMessage(`Player ${this.getPlayerId(this.#controller.winner)} has won!`);
+    } else {
+      this.prompt();
+    }
+  }
+
+  getPlayerId(player) {
+    return this.#players.find((p) => p.player === player).id;
+  }
+
+  getPlayerData(id) {
+    const player = this.#players[id - 1];
+    if (!player) throw new Error(`Invalid player ID: ${id}`);
+    return player;
+  }
+
+  printBoard(id, hideShips = false) {
+    try {
+      const { player } = this.getPlayerData(id);
+      player.printBoard(`Player ${id} Board:`, hideShips);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  placeShip(playerId, type, coordinates, orientation) {
+    try {
+      const { ships, gameboard } = this.getPlayerData(playerId);
+      gameboard.placeShip(ships[type].ship, coordinates, orientation);
+      this.logMessage(`Placed ${type} at [${coordinates[0]}, ${coordinates[1]}]`);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  resetShip(playerId, type) {
+    try {
+      const { ships, gameboard } = this.getPlayerData(playerId);
+      gameboard.resetShip(ships[type].ship);
+      this.logMessage(`Removed ${type} from gameboard.`);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  placeAllShips(playerId) {
+    try {
+      const { ships, gameboard } = this.getPlayerData(playerId);
+      Object.values(ships).forEach(({ ship }) => {
+        gameboard.placeShipRandom(ship);
+      });
+      this.logMessage(`Placed all ships for Player ${playerId}.`);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+
+  resetAllShips(playerId) {
+    try {
+      const { ships, gameboard } = this.getPlayerData(playerId);
+      Object.values(ships).forEach(({ ship }) => {
+        gameboard.resetShip(ship);
+      });
+      this.logMessage(`Reset all ships for Player ${playerId}.`);
+    } catch (error) {
+      this.logMessage(this.formatErrorMsg(error), 'error');
+    }
+  }
+}
+
+export default Battleship;
