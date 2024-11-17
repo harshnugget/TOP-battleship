739423dcf8efd2a7f26ed724ee7b3aae7393cb03
harshnugget <76251920+harshnugget@users.noreply.github.com>
2024-11-17 05:48:28 +0000

    feat(BattleshipUI.js): Add event listeners for drag-and-drop ship placement, rotation and attacking
    - Renamed `getCell` to `getElement` for broader use - now takes a class name and parent element for retrieving the element at clicked position.
    - Implemented `dragstart`, `drag`, and `dragend` event listeners on ships to enable interactive dragging for ship placement on the gameboard.
    - Added `click` event on ships for rotating.
    - Implemented `getCoordinatesByCellIndex` to calculate which coordinates to place the ship at depending on the index of the child element (cell) of the ship clicked on.
    - Gameboard cells now have a `dragenter` event for storing the current cell dragged over, allowing for ship events to interact with it.

--- src/BattleshipUI.js
+++ src/BattleshipUI.js
@@ -3,6 +3,7 @@ import GameboardUI from './ui/GameboardUI.js';
 import ShipUI from './ui/ShipUI.js';
 
 class BattleshipUI extends Battleship {
+  // Containers for the gameboard and ships UI
   static createContainers() {
     const gameboardContainer = document.createElement('div');
     const shipsContainer = document.createElement('div');
@@ -73,6 +74,11 @@ class BattleshipUI extends Battleship {
     this.render();
   }
 
+  rotateShip(playerId, type) {
+    super.rotateShip(playerId, type);
+    this.render();
+  }
+
   resetShip(playerId, type) {
     super.resetShip(playerId, type);
     this.render();
@@ -93,8 +99,7 @@ class BattleshipUI extends Battleship {
     const player2 = super.getPlayerData(2);
 
     [player1, player2].forEach((player) => {
-      const ships = player.ships;
-      const gameboardUI = player.gameboardUI;
+      const { ships, gameboardUI } = player;
 
       gameboardUI.render();
 
@@ -112,33 +117,137 @@ class BattleshipUI extends Battleship {
       const { gameboardUI } = player;
       const gameboard = gameboardUI.gameboardElement;
       const cells = gameboardUI.getAllCells();
+      let cellDraggedOver;
 
-      // Retrieves the gameboard cell at the location of a click
-      const getCell = (event) => {
+      // Retrieves the element, with specified class and parent, at the location of a click
+      const getElement = (event, className, parentElement) => {
         const elements = document.elementsFromPoint(event.clientX, event.clientY);
 
-        const cell = elements.find((element) => {
-          return element.classList.contains('cell') && element.parentElement === gameboard;
+        return elements.find((element) => {
+          return (
+            element.classList.contains(`${className}`) && element.parentElement === parentElement
+          );
         });
-
-        return cell;
       };
 
+      // Sends an attack at the coordinates of the specified gameboard cell
       const attackCell = (cell) => {
-        if (super.winner) {
-          return console.error('Game has ended! Start a new game.');
-        }
-
-        if (player.player !== super.activePlayer && cell) {
+        if (player.player !== super.activePlayer && super.gameInProgress && cell) {
           this.attack(cell.dataset.row, cell.dataset.column);
         }
       };
 
+      const getCoordinatesByCellIndex = (orientation, index) => {
+        /* Calculate the coordinates to place a ship based on the index of the ship cell at the location of dragstart.
+        EXAMPLE:
+        1. A ship of length 4 (4 cells) is oriented horizontally.
+        2. The user begins the dragstart event at the position of the 3rd ship cell, so the index is 3.
+        3. The current gameboard cell dragged over has coordinates [row: 5, column: 9].
+        4. The final placement coordinates adjust the column by subtracting the index (9 - 3 = 6).
+        5. The ship will be placed at [row: 5, column: 6]. */
+
+        if (!cellDraggedOver) return;
+
+        let row = Number(cellDraggedOver.dataset.row);
+        let col = Number(cellDraggedOver.dataset.column);
+
+        if (orientation === 'horizontal') {
+          col -= index;
+        } else if (orientation === 'vertical') {
+          row += index;
+        }
+
+        return [row, col];
+      };
+
+      // Add event listeners to each cell
       cells.forEach((cell) => {
+        // Handle click for attacking at current gameboard cell
         cell.addEventListener('click', (event) => {
-          const cell = getCell(event);
+          const cell = getElement(event, 'cell', gameboard);
           if (cell) {
             attackCell(cell);
+          }
+        });
+
+        // Handle drag enter for tracking the current gameboard cell
+        cell.addEventListener('dragenter', (event) => {
+          const cell = getElement(event, 'cell', gameboard);
+          if (cell) {
+            cellDraggedOver = cell;
+          }
+        });
+      });
+
+      // Add event listeners to each ship
+      Object.keys(player.ships).forEach((type) => {
+        const ship = player.ships[type].ship;
+        const shipUI = player.ships[type].shipUI;
+        const shipElement = shipUI.shipElement;
+        let cellIndex;
+
+        // Ensure the ship element is draggable
+        shipElement.draggable = true;
+
+        // Handle click for ship rotation
+        shipElement.addEventListener('click', () => {
+          try {
+            super.validateAction('rotate ship');
+            this.rotateShip(player.id, type);
+          } catch (error) {
+            super.logMessage(super.formatErrorMsg(error), 'error');
+          }
+        });
+
+        // Handle drag start to begin ship repositioning
+        shipElement.addEventListener('dragstart', (event) => {
+          // Function to remove the ship's current gameboard cells while dragging
+          const removeFromBoard = () => {
+            // Store the ship's current orientation for restoration after resetting
+            const prevOrientation = ship.orientation;
+
+            // Reset the ship and update the gameboard UI, effectively removing the ship from its current position
+            super.resetShip(player.id, type);
+            gameboardUI.render();
+
+            // Restore the ship's previous orientation for consistent repositioning
+            ship.orientation = prevOrientation;
+          };
+
+          try {
+            super.validateAction('drag ship');
+            const cell = getElement(event, 'cell', shipElement);
+            removeFromBoard();
+            cellIndex = [...shipElement.children].findIndex((child) => child === cell);
+          } catch (error) {
+            event.preventDefault();
+            shipElement.draggable = false;
+            super.logMessage(super.formatErrorMsg(error), 'error');
+          }
+        });
+
+        // Handle the dragging for ship placeholders
+        shipElement.addEventListener('drag', () => {
+          try {
+            const [row, col] = getCoordinatesByCellIndex(ship.orientation, cellIndex);
+            shipUI.placeShip([row, col]);
+          } catch (error) {
+            super.logMessage(super.formatErrorMsg(error), 'error');
+          }
+        });
+
+        // Handle drag end to place ship in new position
+        shipElement.addEventListener('dragend', () => {
+          try {
+            const [row, col] = getCoordinatesByCellIndex(ship.orientation, cellIndex);
+            const cell = gameboardUI.getCell([row, col]);
+
+            if (cell) {
+              this.placeShip(player.id, type, [row, col], ship.orientation);
+              cellDraggedOver = null;
+            }
+          } catch (error) {
+            super.logMessage(super.formatErrorMsg(error), 'error');
           }
         });
       });
